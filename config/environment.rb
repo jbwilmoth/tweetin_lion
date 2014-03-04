173b497bb82194502425a3b6eb03dbb6f231d361
BATMAN <nealcpeters@gmail.com>
2014-03-04 13:33:22 -0600

    second commit

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,9 @@ require "sinatra/reloader" if development?
 
 require 'erb'
 
+require 'twitter'
+require 'twitter_oauth'
+
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
